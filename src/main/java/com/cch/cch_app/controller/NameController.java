756f130e5aaf45efe316f68fbaaee18b68a72df0
Case, Jack <jackcase04@gmail.com>
2025-06-22 15:43:44 -0500

    testing connection with react native

--- src/main/java/com/cch/cch_app/controller/NameController.java
+++ src/main/java/com/cch/cch_app/controller/NameController.java
@@ -3,13 +3,14 @@ package com.cch.cch_app.controller;
 import com.cch.cch_app.model.Name;
 import com.cch.cch_app.service.NameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/names")
 public class NameController {
