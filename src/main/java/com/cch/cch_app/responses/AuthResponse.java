9733917dd20d192ef0c88c6e2629ad6a67e7d771
Case, Jack <jackcase04@gmail.com>
2025-07-15 20:49:26 -0500

    made standard response for signup and login

--- src/main/java/com/cch/cch_app/responses/AuthResponse.java
+++ src/main/java/com/cch/cch_app/responses/AuthResponse.java
@@ -5,13 +5,15 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class LoginResponse {
+public class AuthResponse {
     private String fullname;
+    private String username;
     private String token;
     private long expiresIn;
 
-    public LoginResponse(String fullname, String token, long expiresIn) {
+    public AuthResponse(String fullname, String username, String token, long expiresIn) {
         this.fullname = fullname;
+        this.username = username;
         this.token = token;
         this.expiresIn = expiresIn;
     }
